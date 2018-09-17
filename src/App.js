bc8ef5b0c069e0acb9d97e0127360e1f23da78d0
Walker Lyle <walker@walkers-air.lan>
2018-09-17 17:06:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
+import AreaChart from "./components/AreaChart";
+import DonutChart from "./components/DonutChart";
+import Dashboard from "./components/Dashboard";
 
-function App() {
+import {
+  BrowserRouter,
+  Route,
+  Switch
+ } from "react-router-dom";
+
+function App(props) {
   return (
+    <BrowserRouter>
     <div> 
       <div id="wrapper">
         <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -11,10 +21,30 @@ function App() {
           <SideNav />
         </nav>
         <div style={{backgroundColor: "white"}}>
-          {/* PUT YOUR ROUTES HERE */}
+          {/* PUT YOUR ROUTES HERE 
+          * /              -> Dashboard
+* /charts        -> Charts
+* /tables        -> Tables
+* /settings      -> Settings
+* /wall          -> Wall
+* /profiles      -> Profiles
+* /marquee/:text -> Marquee
+* /profile/:id   -> Profile*/}
+          <Switch>
+           <Route path="/" component={Dashboard} />
+           <Route path="/charts" render={(props) => {
+             return (
+               <div>
+                 <AreaChart />
+                 <DonutChart />
+               </div>
+             );
+           }} />
+           </Switch>
         </div>
       </div>
     </div>
+    </BrowserRouter>
 
   );
 }
